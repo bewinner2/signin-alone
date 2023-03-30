8e9136eac55e4a6aa41adc0c0ab9c99f7fec4f50
bewinner2 <bewinner2@gmail.com>
2023-03-30 13:42:07 +0900

    console.log added

--- app.js
+++ app.js
@@ -11,11 +11,6 @@ const i18nextMiddleware = require('i18next-http-middleware');
 mongoose.connect('mongodb+srv://bewinner2:59595653q!@cluster0.qcwsa7a.mongodb.net/test2');
 const Person = mongoose.model('person', { name: String, email: String });
 
-// const apiKey = 'f38b94838e27678bb011d472743537af-us21';
-// const listId = 'a4d2d3c9d3';
-// const serverPrefix = 'us21';
-// const url = `https://us21.api.mailchimp.com/3.0/lists/a4d2d3c9d3/members`;
-
 const app = express();
 
 // // Use i18next middleware
@@ -37,27 +32,12 @@ app.post('/', function (req, res) {
     const email = req.body.email; // Extract email from form submission
     const name = req.body.name;
 
+    console.log(email);
+    console.log(name);
+
+
     const human = new Person({ name: name, email: email });
     human.save().then(() => console.log('WOW!! '));
-
-    // axios.post(url, {
-    //     email_address: email,
-    //     status: 'subscribed',
-    //     merge_fields: {
-    //         FNAME: name
-    //     }
-    // }, {
-    //     auth: {
-    //         username: 'anystring',
-    //         password: "f38b94838e27678bb011d472743537af-us21"
-    //     }
-    // })
-    //     .then(response => {
-    //         console.log('Member added:', response.data);
-    //     })
-    //     .catch(error => {
-    //         console.log('Error:', error.response.data);
-    //     });
 
     console.log(`Received sign-up request for email ${email}`); // Log the email address
     // Send response to client
